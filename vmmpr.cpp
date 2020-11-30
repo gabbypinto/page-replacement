7c4253a4f7b57edf1cfbc18eff2ef6ab3cbcc6f0
Katherine Hansen <katherinehansen@Katherines-MacBook-Air.local>
2020-11-30 13:22:29 -0800

    commented some things out

--- vmmpr.cpp
+++ vmmpr.cpp
@@ -20,7 +20,7 @@ int fifo(char *pages[]){
     //cout<<pages[1];
     for(int i=0; i<strlen(pages[1]);i++){
         if(pages[1][i]=='0'||pages[1][i]=='1'||pages[1][i]=='2'||pages[1][i]=='3'||pages[1][i]=='4'||pages[1][i]=='5'||pages[1][i]=='6'||pages[1][i]=='7'||pages[1][i]=='8'||pages[1][i]=='9'){
-            cout<<"page:"<<pages[1][i]<<"\n"<<endl;
+            
             if(current.size()<NUM_PAGES){
                 if(current.find(pages[1][i])==current.end()){
                     current.insert(pages[1][i]);
@@ -40,68 +40,65 @@ int fifo(char *pages[]){
                 }
             }
         }
-        else{
-            cout<<"Not a num: "<<pages[1][i]<<"\n"<<endl;
-        }
     }
         
     return pageFaults;
 }
-bool checkPageTable(char pageTable[], char element){
-    for(int i=0;i<sizeof(pageTable);i++){
-        if(pageTable[i]==element)
-            return true;
-    }
-    return false;
-}
-int lru(char *pages[]){
-    unordered_set<char> current;
-    //unordered_map<char,int> pageTable;
-    char pageTable[3];
-    for(int i=0;i<3;i++){
-        pageTable[i]=' ';
-    }
-    int pageFaults=0;
-    for(int i=0; i<strlen(pages[1]);i++){
-        // if(current.size()<NUM_PAGES){
-        //     if(current.find(pages[1][i])==current.end()){
-        //         current.insert(pages[1][i]);
-        //         pageFaults++;
-        //     }
-        //     pageTable[pages[1][i]]==i;
-        // }
-        // else{
-        //     if(current.find(pages[1][i])==current.end()){
+// bool checkPageTable(char pageTable, char element){
+//     for(int i=0;i<sizeof(pageTable);i++){
+//         if(pageTable[i]==element)
+//             return true;
+//     }
+//     return false;
+// }
+// int lru(char *pages[]){
+//     unordered_set<char> current;
+//     //unordered_map<char,int> pageTable;
+//     char pageTable[3];
+//     for(int i=0;i<3;i++){
+//         pageTable[i]=' ';
+//     }
+//     int pageFaults=0;
+//     for(int i=0; i<strlen(pages[1]);i++){
+//         if(current.size()<NUM_PAGES){
+//             if(current.find(pages[1][i])==current.end()){
+//                 current.insert(pages[1][i]);
+//                 pageFaults++;
+//             }
+//             pageTable[pages[1][i]]=i;
+//         }
+//         else{
+//             if(current.find(pages[1][i])==current.end()){
                 
-        //     }
-        // }
-        if(!checkPageTable(pageTable, pages[1][i])){//not in page table
-            for(int i=0;i<sizeof(pageTable);i++){
-                if(pageTable[i]==' ')
-                    pageTable[i]=pages[1][i];
-                else{
-                    for(int i=0;i<sizeof(pageTable)-1;i++){
-                        pageTable[i]=pageTable[i+1];
-                    }
-                    pageTable[sizeof(pageTable)-1]=pages[1][i];
-                }
-            }
-        }
-        else{//already in page table
-            for(int i=0;i<sizeof(pageTable)-1;i++){
-                if(pageTable[i]==pages[1][i]){//found current location of page
-                    for(int j=i;j<sizeof(pageTable)-1;j++){
-                        pageTable[j]=pageTable[j+1];
-                    }
-                    pageTable[sizeof(pageTable)-1]=pages[1][i];
-                    break;
-                }
-            }
-        }
+//             }
+//         }
+//         if(!checkPageTable(pageTable, pages[1][i])){//not in page table
+//             for(int i=0;i<sizeof(pageTable);i++){
+//                 if(pageTable[i]==' ')
+//                     pageTable[i]=pages[1][i];
+//                 else{
+//                     for(int i=0;i<sizeof(pageTable)-1;i++){
+//                         pageTable[i]=pageTable[i+1];
+//                     }
+//                     pageTable[sizeof(pageTable)-1]=pages[1][i];
+//                 }
+//             }
+//         }
+//         else{//already in page table
+//             for(int i=0;i<sizeof(pageTable)-1;i++){
+//                 if(pageTable[i]==pages[1][i]){//found current location of page
+//                     for(int j=i;j<sizeof(pageTable)-1;j++){
+//                         pageTable[j]=pageTable[j+1];
+//                     }
+//                     pageTable[sizeof(pageTable)-1]=pages[1][i];
+//                     break;
+//                 }
+//             }
+//         }
 
-    }
-    return pageFaults;
-}
+//     }
+//     return pageFaults;
+// }
 
 
 int main(int argc, char *argv[]){
@@ -132,7 +129,7 @@ int main(int argc, char *argv[]){
     //     cout<<pages[count]<<"\n"<<endl;
     // }
 
-    int numFaults=lru(argv);
-    cout<<"NumFaults:"<<numFaults<<"\n"<<endl;
+    int numFaults=fifo(argv);
+    cout<<"Number of Faults:"<<numFaults<<endl;
 
  }
